f8ce4f84b6882ea544fec2c25f6c83ea1a8023fc
cursedxc <108552975+cursedxc@users.noreply.github.com>
2022-07-01 23:38:33 +0200

    Add files via upload

---
+++ package detect.lua
@@ -0,0 +1,6 @@
+if gg.isPackageInstalled('package') then 
+    gg.alert('uninstall detect package') os.exit() 
+    gg.processKill() 
+end
+
+   \\I will save the codes for myself every day, maybe someone will need it
